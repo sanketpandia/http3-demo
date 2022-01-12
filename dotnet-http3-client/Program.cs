2755fbb7def589da9868f4159fa22a58d065fdaa
Sanket Pandia <sanketpandia@gmail.com>
2022-01-12 15:39:25 +0530

    Initial Commit

---
+++ dotnet-http3-client/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+var client = new HttpClient();
+client.DefaultRequestVersion = HttpVersion.Version30;
+client.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact;
+
+var resp =  await client.GetAsync("https://localhost:5001");
+var body = await resp.Content.ReadAsStringAsync();
+
+System.Console.WriteLine($"Status: {resp.StatusCode}, version: {resp.Version}");
