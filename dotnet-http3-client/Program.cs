4d80b2bf15232b94a15e35542899a8935453b258
TeBeCo (wsl) <TeBeCo@gmail.com>
2022-01-12 14:58:45 +0400

    bad fix: hard return "true" on ServerCertificateCustomValidation

--- dotnet-http3-client/Program.cs
+++ dotnet-http3-client/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 
 using var cts = new CancellationTokenSource();
 Console.CancelKeyPress += (s, e) => cts.Cancel();
 
 while (!cts.Token.IsCancellationRequested)
 {
-    using var client = new HttpClient();
+    using var handler = new HttpClientHandler();
+    handler.ServerCertificateCustomValidationCallback = ServerCertificateCustomValidation;
+
+    using var client = new HttpClient(handler);
 
     client.DefaultRequestVersion = HttpVersion.Version30;
     client.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact;
@@ -20,4 +25,25 @@ while (!cts.Token.IsCancellationRequested)
     Console.WriteLine();
     Console.WriteLine("Waiting 1 second before next request ...");
     await Task.Delay(1000, cts.Token);
+
+    bool ServerCertificateCustomValidation(HttpRequestMessage requestMessage, X509Certificate2? certificate, X509Chain? chain, SslPolicyErrors sslErrors)
+    {
+        // It is possible inpect the certificate provided by server
+        Console.WriteLine($"Requested URI: {requestMessage.RequestUri}");
+        Console.WriteLine($"Effective date: {certificate?.GetEffectiveDateString()}");
+        Console.WriteLine($"Exp date: {certificate?.GetExpirationDateString()}");
+        Console.WriteLine($"Issuer: {certificate?.Issuer}");
+        Console.WriteLine($"Subject: {certificate?.Subject}");
+
+        // Based on the custom logic it is possible to decide whether the client considers certificate valid or not
+        Console.WriteLine("========================================");
+        var consoleColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Errors: {sslErrors}");
+        Console.ForegroundColor = consoleColor;
+        Console.WriteLine("========================================");
+
+        // return sslErrors == SslPolicyErrors.None;
+        return true;
+    }
 }
