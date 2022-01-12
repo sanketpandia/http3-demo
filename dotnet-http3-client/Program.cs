8ebae5ba3d18a0c142404a95526f5891860d1987
TeBeCo (wsl) <TeBeCo@gmail.com>
2022-01-12 14:58:12 +0400

    Add a loop running requests

--- dotnet-http3-client/Program.cs
+++ dotnet-http3-client/Program.cs
@@ -1,10 +1,23 @@
 ﻿using System.Net;
 
-var client = new HttpClient();
-client.DefaultRequestVersion = HttpVersion.Version30;
-client.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact;
+using var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (s, e) => cts.Cancel();
 
-var resp =  await client.GetAsync("https://localhost:5001");
-var body = await resp.Content.ReadAsStringAsync();
+while (!cts.Token.IsCancellationRequested)
+{
+    using var client = new HttpClient();
 
-System.Console.WriteLine($"Status: {resp.StatusCode}, version: {resp.Version}");
+    client.DefaultRequestVersion = HttpVersion.Version30;
+    client.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact;
+
+    var resp = await client.GetAsync("https://localhost:5001");
+    var body = await resp.Content.ReadAsStringAsync();
+
+    Console.WriteLine($"Status: {resp.StatusCode}, version: {resp.Version}");
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("Waiting 1 second before next request ...");
+    await Task.Delay(1000, cts.Token);
+}
